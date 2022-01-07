d4f7efd1d1fe137ec7973a3b3ed8f72f23f7a2d1
venkateshLadderStack <venkatesh@ladderstack.com>
2022-01-07 20:52:56 +0530

    chnages

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -1,11 +1,12 @@
 import React from "react"
+import styled from "styled-components"
 
 const TeamCard = ({ name, title, img }) => {
   return (
     <div className="col-lg-3 col-md-4">
       <div className="team-wrap">
         <div className="team-img">
-          <img src={img} alt="" />
+          <EmployeeImg src={img} alt="" />
         </div>
         <div className="team-content">
           <h5>{name}</h5>
@@ -15,5 +16,12 @@ const TeamCard = ({ name, title, img }) => {
     </div>
   )
 }
+
+const EmployeeImg = styled.img`
+  border-radius: 50%;
+  width: 272px;
+  height: 272px;
+  object-fit: cover;
+`
 
 export default TeamCard
